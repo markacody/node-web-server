acd0f117416cac81352ec70e5902192c858ceab6
Mark Cody <markcody@Marks-MacBook-Pro.local>
2017-12-31 14:51:16 -0600

    projects page added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const hbs = require('hbs');
 const fs = require('fs');
 
-//for heroku: add the port variable with process.env, give port to app.listen, and provide start script to package.json. when done, you can start the app from the command line with npm start  
+//for heroku: add the port variable with process.env.PORT, then give port to app.listen, and provide the start script to package.json. When done, you can start the app from the command line with npm start. To deploy from the command line, create the heroku repo: heroku create; then git push heroku; then heroku open.
 //declare the environment variable that heroku will assign
 const port = process.env.PORT || 3000;
 //create the app server
@@ -19,7 +19,7 @@ hbs.registerHelper('getCurrentYear', () => {
 //configure express using set with key value pairs --http://expressjs.com/en/4x/api.html#app.set-- including what the templating engine and where the views directory
 app.set('view engine', 'hbs');
 
-//add middleware with use then provide the express method or function. First get information about the request.
+//Add middleware with use then provide the express method or function. This first piece of middleware gets information about the request, before you decide how to respond. Add as much middleware as needed. Examples are static files, dynamic content, authentication, and performance tracking. Call next to sequence middleware and keep app going. Without next, user request "hangs, spins, etc" because server has nowhere to go.
 app.use((req, res, next) => {
     var now = new Date().toString();
     var log = `${now}: ${req.method} ${req.url}`;
@@ -35,26 +35,33 @@ app.use((req, res, next) => {
 //    res.render('maintenance.hbs');
 //});
 
-//takes the absolute path to the project. specify the variable __dirname which stores the path to the project. Add as much middleware as needed. Examples are dynamic content, authentication, and performance tracking. Call next to sequence middleware and keep app going. Without next, user request "hangs, spins, etc" because server has nowhere to go.
+//Use works with other express methods to serve files. express.static takes the path to the project. Variable __dirname stores the path to the project. 
 app.use(express.static(__dirname + '/public'));
 
-//when rendering, page the page url and the object
-app.get('/about', (req, res) => {
-    res.render('about.hbs', {
-        dynamicContent: "This is content from an object",
-        welcomeMessage: "Hi Harry"
-    });
-});
-
+//when rendering, provide the file name and an object with prop value pairs. Render is used with templating whereas send is used with static html files.
 app.get('/', (req, res) => {
     res.render('home.hbs', {
+        pageTitle: 'Home',
         dynamicContent: "This is content from an object",
         welcomeMessage: "Hi Harry"
     });
 });
 
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About',
+        dynamicContent: "This is content from an object",
+        welcomeMessage: "Hi Harry"
+    });
+});
 
-//bind the app to a port with listen. IRL this number would be determined dynamically and stored in an environment variable. Listen stops only when told. Listen takes two arguments, the port nbr and a function that executes when the server starts.
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+});
+
+//bind the app to a port with listen. Listen takes two arguments, the port --variable or nbr-- and a function that executes when the server starts.
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
